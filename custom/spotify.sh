aad4bc217160ebdcfc1e00eb4e94807ccbfe5101
Joseph Phillips <joseph@josephphillips.co>
2024-01-04 19:55:03 -0500

    Update spotify.sh

--- custom/spotify.sh
+++ custom/spotify.sh
@@ -1,7 +1,7 @@
 show_spotify() {
   local index=$1
   local icon="$(get_tmux_option "@catppuccin_test_icon" "󰙨")"
-  local color="$(get_tmux_option "@catppuccin_test_color" "$thm_gree")"
+  local color="$(get_tmux_option "@catppuccin_test_color" "$thm_green")"
   local text="$(get_tmux_option "@catppuccin_test_text" " #(~/Documents/spt_status.sh)")"
 
   local module=$( build_status_module "$index" "$icon" "$color" "$text" )
